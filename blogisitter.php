a0e7309c31bd82f6bddba017dac320bc42b49364
arinaturcu <arinaturcu2@gmail.com>
2023-03-03 14:10:13 +0200

    initial commit

---
+++ blogisitter.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Bloggisitter
+Plugin URI:
+Description: Plugin to accompany tutsplus guide to creating plugins, registers a post type.
+Version: 1.0
+Author: Andreea Radacina, Arina Turcu
+License: GPLv2 or later
+*/
+
+use ThemeIsle\Bloggisitter\Main;
+
+define( 'BLOGGISITTER_PATH', dirname( __FILE__ ) );
+
+$vendor_file = BLOGGISITTER_PATH . '/vendor/autoload.php';
+
+if ( is_readable( $vendor_file ) ) {
+	require_once $vendor_file;
+}
+
+if ( class_exists( '\ThemeIsle\Bloggisitter\Main' ) ) {
+	Main::instance();
+}
---
+++ inc/main.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Loader.
+ *
+ * @package ThemeIsle
+ */
+
+namespace ThemeIsle\Bloggisitter;
+
+class Main {
+	/**
+	 * Singleton.
+	 *
+	 * @var Main Class object.
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Method to define hooks needed.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 */
+	public function init() {
+		add_action( 'init', array( $this, 'run' ) );
+	}
+
+	public function run() {
+		echo '<h1>AAAAAAAAAA</h1>';
+	}
+
+	/**
+	 * Singleton method.
+	 *
+	 * @static
+	 *
+	 * @return  Main
+	 * @since   1.0.0
+	 * @access  public
+	 */
+	public static function instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+			self::$instance->init();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Throw error on object clone
+	 *
+	 * The whole idea of the singleton design pattern is that there is a single
+	 * object therefore, we don't want the object to be cloned.
+	 *
+	 * @access  public
+	 * @return  void
+	 * @since   1.0.0
+	 */
+	public function __clone() {
+		// Cloning instances of the class is forbidden.
+		_doing_it_wrong( __FUNCTION__, 'Cheatin&#8217; huh?', '1.0.0' );
+	}
+
+	/**
+	 * Disable unserializing of the class
+	 *
+	 * @access  public
+	 * @return  void
+	 * @since   1.0.0
+	 */
+	public function __wakeup() {
+		// Unserializing instances of the class is forbidden.
+		_doing_it_wrong( __FUNCTION__, 'Cheatin&#8217; huh?', '1.0.0' );
+	}
+}
